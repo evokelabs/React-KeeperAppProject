123f9a77c009c85f3f7c654b734e3bc04353e690
Adrian Pikios <adrian@evokelabs.com>
2022-02-08 01:47:05 +1100

    Keeper App Project Framework Added

---
+++ src/component/Keeper.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Keeper(){
+    return (
+        <div>
+        <header>
+        <h1>Keeper</h1>
+        </header>
+
+       
+
+        <footer>
+        <p>Copyright 2023</p>
+        </footer>
+    </div>
+    );
+}
+
+export default Keeper;
