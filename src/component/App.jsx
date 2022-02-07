0f310736d779df11799fa69dfeccef7f474d28b0
Adrian Pikios <adrian@evokelabs.com>
2022-02-08 06:22:17 +1100

    Arrow Function and Dynamic Update

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -2,14 +2,23 @@ import React from "react";
 import Keeper from "./Keeper";
 import Header from "./Header";
 import Note from "./Note";
+import Notes from "../Notes"
 import Footer from "./Footer";
+
 
 
 function App(){
     return (
         <div>
         <Header />
-        <Note />
+        {Notes.map(noteItem => (
+            <Note
+            key={noteItem.key}
+            title={noteItem.title}
+            content={noteItem.content}
+            />
+        ))}
+       
         <Footer />
         </div>
         );
